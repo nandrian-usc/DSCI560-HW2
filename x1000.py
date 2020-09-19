4d3cdeb450980e63cc3807ac4ac257fdb8461766
nandrian-usc <70176510+nandrian-usc@users.noreply.github.com>
2020-09-18 21:23:17 -0700

    Update x1000.py

--- x1000.py
+++ x1000.py
@@ -2,11 +2,7 @@ import sys
 from random import randint
 
 def main():
-    if (len(sys.argv) != 2):
-        print("Usage: x1000.py <output_file>")
-        sys.exit(-1)
-  
-    output_file = sys.argv[1]
+    output_file = "x.csv"
     l = list()
     for _ in range(1000):
         l.append(str(randint(0, 101)))
