9345140b1c623a45d7f3a156f6e3ea6ab6b4083e
unknown <nandrian@usc.edu>
2020-10-26 00:52:32 -0700

    update script of x and y generator to show initial and intermediate visualization

--- x1000.py
+++ x1000.py
@@ -5,12 +5,23 @@ def main():
     output_file = "1000_x-axis_random_number_ranging_0_to_100.csv"
     l = list()
     for _ in range(1000):
-        l.append(str(randint(0, 101)))
+        l.append(randint(0, 100))
+        
+    #setNumber = {n for n in sorted(l)}
+    #print(setNumber)
+    dictNumber = {}
+    for n in sorted(l):
+        if n not in dictNumber: dictNumber[n] = 0
+        dictNumber[n] = dictNumber[n] + 1   
+        
         
     with open(output_file, "w") as resultFile:
-        data = "\n".join(l)
+        data = "\n".join([str(n) for n in l])
         resultFile.write(data)
-    print("Generate 1000 random numbers")
+    print("Generate 1000 random numbers for x-axis ranging in 0-100.")
+    print("Random number distribution -> (Key number : # count of number)")
+    for (idx, cnt) in dictNumber.items():
+        print(str(idx) + " : " + str(cnt))
 
 if __name__ == '__main__':
     main()
--- y1000.py
+++ y1000.py
@@ -5,12 +5,23 @@ def main():
     output_file = "1000_y-axis_number_by_3x_plus_6.csv"
     
     with open(input_file, "r") as f:
-        l = [str(int(s.replace("\n","")) * 3 + 6) for s in f]
-        
+        l = [int(s.replace("\n","")) * 3 + 6 for s in f]
+    
+    
+    dictNumber = {}
+    for n in sorted(l):
+        if n not in dictNumber: dictNumber[n] = 0
+        dictNumber[n] = dictNumber[n] + 1   
+    
+    
     with open(output_file, "w") as resultFile:
-        data = "\n".join(l)
+        data = "\n".join([str(n) for n in l])
         resultFile.write(data)
-    print("Generate 1000 y numbers")
+        
+    print("Generate 1000 y-axis numbers using y = 3x + 6 formula")
+    print("y-axis number distribution -> (Key number : # count of number)")
+    for (idx, cnt) in dictNumber.items():
+        print(str(idx) + " : " + str(cnt))
 
 if __name__ == '__main__':
     main()
